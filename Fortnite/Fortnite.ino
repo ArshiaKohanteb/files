db89a03afea883b262eb7e3870a19ecf36111120
Arshia Kohanteb <arshiakohanteb@gmail.com>
2018-09-14 13:08:46 -0700

    hi

--- Fortnite/Fortnite.ino
+++ Fortnite/Fortnite.ino
@@ -1,21 +1,25 @@
-int red=A0;
-int yellow=A1;
-int green=A2;
-int timer = 0;
+int red=10;
+int yellow=9;
+int green=11;
+float timer1 = 0;
+float timer2 = 50;
+float timer3 = 100;
 void setup() {
+  Serial.begin(9600);
   pinMode(red, OUTPUT);
   pinMode(yellow, OUTPUT);
   pinMode(green, OUTPUT);
- 
-
 }
 
 void loop() {
- if (timer > 1023)
- {
-  timer = 0;
-  }
-
-analogWrite(red, timer);
- timer += 10;
+  Serial.println(analogRead(0));
+ if (timer1 >= 255) {timer1 = 0;}
+ if (timer2 >= 255) {timer2 = 0;}
+ if (timer3 >= 255) {timer3 = 0;}
+analogWrite(red,timer1);
+analogWrite(yellow, timer2);
+analogWrite(green, timer3);
+ timer1+= .3;
+ timer2+= .3;
+ timer3+= .3; 
 }
